8d85d3153d95b59d3827765c1421273cd0ec2e48
amaan8429 <mohdamaan8429@gmail.com>
2024-09-15 23:19:24 +0530

    analyitcs added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/lib/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system">
           {children}
+          <Analytics />
           <Toaster />
         </ThemeProvider>
       </body>
